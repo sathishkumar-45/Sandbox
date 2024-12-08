b97b1cd7e4970557fc881ee19933ca0d15226d1f
sathishkumar-45 <sathishk24416@gmail.com>
2024-12-08 11:13:24 +0530

    this is single page website completed.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+<script src="https://cdn.emailjs.com/dist/email.min.js"></script>
+<script>
+  (function () {
+    emailjs.init("YOUR_USER_ID"); // Replace with your EmailJS user ID
+  })();
+</script>
